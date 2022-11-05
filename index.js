4b89675170b1b1c5dabf2efae93c2aad887d2ee0
Jay Misal <jaymisal200@gmail.com>
2022-11-05 20:16:35 +0530

    done

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require('path');
 const route1 = require('./routes/route');
 const val = require('./mongoconnection/mongos.js');
-const cors = require('cors');
+// const cors = require('cors');
 val();
 
 app.use(express.json());
 // app.use(cors({credentials:true,origin:"https://starlit-malasada-df2b99.netlify.app"}));
-app.use(cors());
+// app.use(cors());
 
 // app.get("/",(req,res)=>{
 //   res.json("server start")
@@ -19,12 +19,12 @@ app.use(cors());
 app.use(route1);
 
 // SERVER PUSHING TO HEROKU
-// if(process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'));
-//     app.get('*',(req,res) => {
-//       res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-//     });
-// }
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*',(req,res) => {
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
